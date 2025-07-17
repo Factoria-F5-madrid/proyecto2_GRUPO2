c824f68bf6f993e51891eb70279ecf3199ff7c0f
Anthonycpcode <Anthonycp.code@gmail.com>
2025-07-17 14:33:34 +0200

    cambios urls.py

---
+++ crudflorist/floristapp/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from . import views_app
+from . import views_admin
+from . import views_auth
+
+urlpatterns = [
+    # VISTA PRINCIPAL (BIENVENIDA)
+    path("", views_auth.home, name="home"),
+    # AUTENTICACIÓN
+    path("login/", views_auth.login_view, name="login"),
+    path("logout/", views_auth.logout_view, name="logout"),
+    path("signup/", views_auth.signup_view, name="signup"),
+    # VISTAS DE USO GENERAL (USUARIOS)
+    path("productos/", views_app.producto_list, name="producto_list"),
+    path("productos/nuevo/", views_app.producto_create, name="producto_create"),
+    path("productos/<int:pk>/", views_app.producto_detail, name="producto_detail"),
+    path(
+        "productos/<int:pk>/editar/", views_app.producto_update, name="producto_update"
+    ),
+    path("movimientos/", views_app.movimiento_list, name="movimiento_list"),
+    path("movimientos/nuevo/", views_app.movimiento_create, name="movimiento_create"),
+    # VISTAS ADMINISTRATIVAS
+    path("admin/dashboard/", views_admin.dashboard_admin, name="dashboard_admin"),
+    path(
+        "admin/alertas-stock/",
+        views_admin.alertas_bajo_stock,
+        name="alertas_bajo_stock",
+    ),
+    path("admin/reporte-stock/", views_admin.reporte_stock, name="reporte_stock"),
+]
